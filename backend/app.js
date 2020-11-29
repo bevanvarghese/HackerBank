66275d5a28f4a451d41c0305a06b4993785158fd
bevanvarghese <bevanvar@connect.hku.hk>
2020-11-29 23:46:49 +0530

    user routes #3

--- backend/app.js
+++ backend/app.js
@@ -2,27 +2,29 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/hackerbank', (err) => {
-//   if (err)
-//     console.log("MongoDB connection error: "+err);
-//   else 
-//     console.log("Connected to MongoDB");
-// });
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/hackerbank', (err) => {
+  if (err)
+    console.log("MongoDB connection error: "+err);
+  else 
+    console.log("Connected to MongoDB");
+});
 
 app.get('/', (req,res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
-// const userRouter = require('./routes/users');
+const { register, login } = require('./routes/users');
 // const questionRouter = require('./routes/questions');
 // const answerRouter = require('./routes/answers');
 
-//app.use('/route', deconstructedRouteFromAbove);
+//user routes
+app.post('/register', register);
+app.post('/login', login);
 
 app.listen(8000, () => {
   console.log('Server listening on port 8000.');
 });
-
-module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,49 @@
-const router = require('expresss').Router();
+const router = require('express').Router();
 let User = require('../models/user_model');
 
-// exports.register = (req, res) => {};
-// exports.login = (req, res) => {};
+exports.register = (req, res) => {
+  User
+    .find({email: req.body.email})
+    .then((result) => {
+      if(result.length>0) {
+        return res.status(400).json({ error: "Duplicated user's email address" });
+      } 
+      else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        newUser.save() 
+          .then(() => res.status(200).json({ message: 'Account successfully created.' }))
+          .catch(err => res.status(400).json({ error: err }));
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err});
+    });
+};
 
+exports.login = (req, res) => {
+  const user = {
+    email: req.body.enail,
+    password: req.body.password,
+  };
+  User
+    .find({email: req.body.email})
+    .then(result => {
+      if(result.length==0) {
+        return res.status(400).json({ error: "User is not registered."});
+      } 
+      else {
+        if(result[0]["password"]==user.password) {
+          return res.status(200).json({ message: 'Login successful.' });
+        } else {
+          return res.status(400).json({ error: 'Unauthorized access.' });
+        }
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err});
+    });
+}
